ba3a3cd359986ba16261a6253babe4d42bade100
github username <newmanbradm@gmail.com>
2019-01-02 11:03:53 -0500

    add reverse string solution

---
+++ reversestring/reversestring.js
@@ -0,0 +1,15 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    let newStr = ''
+    for (let char of str) {
+        newStr = char + newStr
+    }
+    return newStr
+}
