2e93eccdaa400c60df603843f622d051dd2da477
github username <newmanbradm@gmail.com>
2019-01-12 13:26:44 -0500

    add my solution

--- capitalize/capitalize.js
+++ capitalize/capitalize.js
@@ -6,3 +6,9 @@
 //   capitalize('a short sentence') --> 'A Short Sentence'
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+function capitalize(str) {
+    return str.split(" ").map(word => {
+        return word[0].toUpperCase() + word.slice(1);
+    }).join(" ");
+}
