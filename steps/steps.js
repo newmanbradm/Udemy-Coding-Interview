3bf385bb29dce7ef6693957e493c04ff87179bc8
github username <newmanbradm@gmail.com>
2019-01-12 14:01:41 -0500

    add my solution to steps

--- steps/steps.js
+++ steps/steps.js
@@ -16,3 +16,18 @@
 //       '##  '
 //       '### '
 //       '####'
+
+function steps(n) {
+    for (row = 0; row < n; row++) {
+        let stair = '';
+
+        for (column = 0; column < n; column++) {
+            if (column <= row) {
+                stair += '#';
+            } else {
+                stair += ' ';
+            }
+        }
+        console.log(stair)
+    }
+}
