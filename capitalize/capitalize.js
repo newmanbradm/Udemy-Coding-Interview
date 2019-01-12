379a5cd827241b89d314cdd11517320d751e2e0a
github username <newmanbradm@gmail.com>
2019-01-12 13:42:09 -0500

    add instructor solutions

--- capitalize/capitalize.js
+++ capitalize/capitalize.js
@@ -12,3 +12,29 @@ function capitalize(str) {
         return word[0].toUpperCase() + word.slice(1);
     }).join(" ");
 }
+
+// instructor solution(s)
+function capitalize(str) {
+    const words = [];
+
+    for (let word of str.split(' ')) {
+        word.push(word[0].toUpperCase() + word.slice(1));
+    }
+
+    return words.join(' ');
+}
+
+// has the issue of assuming the first character will always be capitalized
+function capitalize(str) {
+    let result = str[0].toUpperCase();
+
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1] === ' ') {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i];
+        }
+    }
+
+    return result;
+}
