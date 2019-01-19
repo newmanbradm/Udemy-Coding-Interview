71028f9036fd1709c323754bf941ba150610c100
github username <newmanbradm@gmail.com>
2019-01-19 11:43:31 -0500

    add recursive solution for steps

--- steps/steps.js
+++ steps/steps.js
@@ -31,3 +31,22 @@ function steps(n) {
         console.log(stair)
     }
 }
+
+// add recursive solution
+function steps(n, row = 0, stair = '') {
+    if (n === row) {
+        return;
+    }
+
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+    steps(n, row, stair);
+}
