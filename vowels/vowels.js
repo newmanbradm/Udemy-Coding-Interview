bf5891df79a8028d409abb3bbf82df948ae53815
github username <newmanbradm@gmail.com>
2019-01-26 13:58:17 -0500

    add instructor iterative solution

--- vowels/vowels.js
+++ vowels/vowels.js
@@ -10,11 +10,25 @@
 // my solution
 function vowels(str) {
     let counter = 0;
-    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let vowelArr = ['a', 'e', 'i', 'o', 'u']
     for (let char of str.toLowerCase()) {
-        if (vowels.includes(char)) {
-            counter += 1
+        if (vowelArr.includes(char)) {
+            counter += 1;
         }
     }
     return counter;
+}
+
+// instructors iterative solution
+function vowels(str) {
+    let count = 0;
+    const checker = 'aeiou';
+
+    for (let char of str.toLowerCase()) {
+        if (checker.includes(char)) {
+            count++;
+        }
+    }
+
+    return count;
 }
